1ec0e0db86bc4a65c4ece0c9227fa4aa09f1d8f8
vsuganya26 <106871256+vsuganya26@users.noreply.github.com>
2022-06-19 06:53:52 -0700

    Add files via upload

--- ITSupportAdmin/src/com/lab1/service/EmployeeCredentialService.java
+++ ITSupportAdmin/src/com/lab1/service/EmployeeCredentialService.java
@@ -5,12 +5,17 @@ import com.lab1.model.Employees;
 public class EmployeeCredentialService {
 
 	String companyName = "gl";
-	
+
 	Employees isa = new Employees();
 	PasswordGenerator pg = new PasswordGenerator();
 
+	/** This method generates the username based on the below parameters and the password is gnerated
+	 * @param firstName
+	 * @param lastName
+	 * @param department
+	 */
 	public void generateCredentials(String firstName, String lastName, String department) {
-		
+
 		String userName = firstName + lastName + "@" + department + "." + companyName + ".com";
 		String passWord = pg.generatePassword();
 
@@ -20,10 +25,13 @@ public class EmployeeCredentialService {
 		isa.setLastName(lastName);
 	}
 
+	/**
+	 * This method displays the user credentials
+	 */
 	public void displayCredentials() {
 		System.out.println("Dear" + " " + isa.getFirstName() + " " + "your generated credentials are as follows");
-		System.out.println("Email --->" + " "+ isa.getUserName());
-		System.out.println("Password --->" + " "  + isa.getPassWord());
+		System.out.println("Email --->" + " " + isa.getUserName());
+		System.out.println("Password --->" + " " + isa.getPassWord());
 	}
-	
+
 }
